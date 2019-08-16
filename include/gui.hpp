068e7779a354a7d221216faa3671cff55a60f8c9
thermonuke_ <nikhilrdave2003@gmail.com>
2019-08-16 11:36:10 -0700

    Starting work on a GUI for debug and auton selection

---
+++ include/gui.hpp
@@ -0,0 +1,22 @@
+#pragma once
+#include "main.h"
+
+
+
+class gui {
+    private: 
+        lv_obj_t * tabview;
+
+        lv_obj_t * tabAuton;
+
+        lv_obj_t * tabDebug;
+
+        lv_obj_t * header;
+    public:
+
+        gui();
+
+        
+
+
+};
