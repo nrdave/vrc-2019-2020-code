5102121cdf0d3a107b1b1a6c346d2e4665e7b91c
thermonuke_ <nikhilrdave2003@gmail.com>
2019-08-18 20:49:57 -0700

    Yes

--- src/gui.cpp
+++ src/gui.cpp
@@ -10,7 +10,7 @@ GUI::GUI(){
     tabDebug = lv_tabview_add_tab(tabview, "Debug Menu");
 
     guiHeader = lv_label_create(lv_scr_act(), NULL);
-    lv_label_set_text(guiHeader, "nightGUI v0.1.0");
+    lv_label_set_text(guiHeader, "sharkUI v0.1.0");
     lv_obj_align(guiHeader, NULL, LV_ALIGN_IN_TOP_LEFT, 0, 0);
 
     static const char * autonMap[] = {"RFRONT", "BFRONT", "\n", "RBACK", "BBACK", "\n", "NOAUTO", ""};
