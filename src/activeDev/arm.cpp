0e9d0ca42edeb188d726667dfd03b827f77786ff
Nikhil <nikhilrdave2003@gmail.com>
2020-01-07 14:43:00 -0700

    Changing the arm and reassigning the ports

--- src/activeDev/arm.cpp
+++ src/activeDev/arm.cpp
@@ -1,43 +1,33 @@
 #include "main.h"
 
-Arm::Arm(): leftArmMotor(ARM_LMOTOR_PORT), rightArmMotor(ARM_RMOTOR_PORT){
-    okapi::Motor leftArmMotor(ARM_LMOTOR_PORT, true, 
+Arm::Arm(): armMotor(ARM_MOTOR_PORT){
+    okapi::Motor armMotor(ARM_MOTOR_PORT, true, 
     okapi::AbstractMotor::gearset::green, okapi::AbstractMotor::encoderUnits::degrees);
 
-    leftArmMotor.setBrakeMode(okapi::AbstractMotor::brakeMode::hold);    
-
-    okapi::Motor rightArmMotor(ARM_RMOTOR_PORT, false, 
-    okapi::AbstractMotor::gearset::green, okapi::AbstractMotor::encoderUnits::degrees);
-    
-    rightArmMotor.setBrakeMode(okapi::AbstractMotor::brakeMode::hold);
+    armMotor.setBrakeMode(okapi::AbstractMotor::brakeMode::hold);        
 }
 
 void Arm::driver(okapi::Controller controller) {
 
         if(controller.getDigital(okapi::ControllerDigital::X)) {
-            leftArmMotor.moveAbsolute(15,100);
-            rightArmMotor.moveAbsolute(15,100);            
+            armMotor.moveAbsolute(15,100);
         }
         if(controller.getDigital(okapi::ControllerDigital::L1)){
-            leftArmMotor.moveVelocity(ARM_MOTOR_SPEED);
-            rightArmMotor.moveVelocity(ARM_MOTOR_SPEED);
+            armMotor.moveVelocity(ARM_MOTOR_SPEED);
         }
         else if(controller.getDigital(okapi::ControllerDigital::L2)){
-            leftArmMotor.moveVelocity(-ARM_MOTOR_SPEED);
-            rightArmMotor.moveVelocity(-ARM_MOTOR_SPEED);
+            armMotor.moveVelocity(-ARM_MOTOR_SPEED);
         }
         else{
-            leftArmMotor.moveVelocity(0);
-            rightArmMotor.moveVelocity(0);
+            armMotor.moveVelocity(0);
         }
 }
 
 void Arm::moveTo(double input){
-    //converting degrees into motor encoder units
+    //accounting for the arm gear ratio
     input *= 5;
     //making sure the arm doesn't move more than 180 degrees
     if(input < 180){
-        leftArmMotor.moveAbsolute(input, 100);
-        rightArmMotor.moveAbsolute(input, 100);
+        armMotor.moveAbsolute(input, 100);
     }
 }
