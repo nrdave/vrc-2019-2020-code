5c871f738038bc3fa4e2115ed4793a5e115df06a
Nikhil <nikhilrdave2003@gmail.com>
2020-02-01 21:14:16 -0700

    Redoing the chassis class so it works

--- include/chassis.hpp
+++ include/chassis.hpp
@@ -1,6 +1,6 @@
 /**
  * The header file for the chassis class. This class is used for
- * controlling the chassis
+ * controlling the base wheels
  */ 
 
 #pragma once
@@ -10,29 +10,18 @@
 #define CHASSIS_MOTORPORT_LF 12
 #define CHASSIS_MOTORPORT_RF 19
 #define CHASSIS_MOTORPORT_LB 11
-#define CHASSIS_MOTORPORT_RB 20
+#define CHASSIS_MOTORPORT_RB 18
 
 class Chassis {
     private:
         /**
-        * the okapi chassis controller object, used for controlling the 
-        * robot in driver
-        */
-        okapi::ChassisControllerIntegrated chassis;
-
-
-        /**
-         * okapi motor objects for each  motor, used in some 
-         * Chassis member functions
+         * okapi motor groups for each  side
          */ 
-        okapi::Motor rfMotor;
+        okapi::MotorGroup rightBase;
 
-        okapi::Motor rbMotor;
+        okapi::MotorGroup leftBase;
 
-        okapi::Motor lfMotor;
-
-        okapi::Motor lbMotor;
-
+        static okapi::ChassisControllerIntegrated autonControl;
     public:
         /**
         * The constructor for the chassis class
@@ -48,27 +37,17 @@ class Chassis {
         * The function used to control the robot in opcontrol
         */ 
         void driver(okapi::Controller controller);
-        /**
-         * A function used in auton for turning to a specific angle
-         * This is just a passthrough of the okapi::turnAngle function,
-         * as the okapi chassis object is private to the chassis class
-         */ 
-
-        void turnAngle(okapi::QAngle angle);
-        /**
-         * This function is, as it name says, a function for making the robot move a specific distance
-         * in autonomous. Unlike turnAngle, this function is more than a passthrough for the
-         * okapiLib function of the same name. 
-         * The first parameter, distance, is what you might think. The 
-         * distance to travel (Using okapiLib's unit literals, I can enter a real distance, such as 10 inches)
-         * The second parameter, velocity, determines the maximum velocity the chassis can run at
-         */ 
-        void moveDistance(okapi::QLength distance, double velocity);
 
         /**
-         * 
+        */ 
+        void moveDistance(float leftTarg, float rightTarg);
+
+        void turnAngle(float angle);
+
+        /**
+         * -=-=-=-  NOT IMPLEMENTED YET -=-=-=-
          * 
          * 
          */
-        void moveSide(char side, double arcLength); 
+        void setVelocity(int velo);
 };
--- src/activeDev/chassis.cpp
+++ src/activeDev/chassis.cpp
@@ -6,56 +6,66 @@
 
 #include "main.h"
 
-Chassis::Chassis(): chassis(okapi::ChassisControllerFactory::create(
-    {CHASSIS_MOTORPORT_LF, CHASSIS_MOTORPORT_LB},
-    {-CHASSIS_MOTORPORT_RF, -CHASSIS_MOTORPORT_RB},
-    okapi::AbstractMotor::gearset::green, {4_in, 13.25_in})),
-    rfMotor(CHASSIS_MOTORPORT_RF), 
-    lfMotor(CHASSIS_MOTORPORT_LF), 
-    rbMotor(CHASSIS_MOTORPORT_RB), 
-    lbMotor(CHASSIS_MOTORPORT_LB) 
+Chassis::Chassis():rightBase({okapi::Motor(CHASSIS_MOTORPORT_RF, 
+                true, okapi::AbstractMotor::gearset::green,
+                okapi::AbstractMotor::encoderUnits::degrees), 
+                okapi::Motor(CHASSIS_MOTORPORT_RB, 
+                true, okapi::AbstractMotor::gearset::green,
+                okapi::AbstractMotor::encoderUnits::degrees)}), 
+                leftBase({okapi::Motor(CHASSIS_MOTORPORT_LF, 
+                false, okapi::AbstractMotor::gearset::green,
+                okapi::AbstractMotor::encoderUnits::degrees), 
+                okapi::Motor(CHASSIS_MOTORPORT_LB, 
+                false, okapi::AbstractMotor::gearset::green,
+                okapi::AbstractMotor::encoderUnits::degrees)})
     {
+        okapi::MotorGroup rightBase({okapi::Motor(CHASSIS_MOTORPORT_RF, 
+                true, okapi::AbstractMotor::gearset::green,
+                okapi::AbstractMotor::encoderUnits::degrees), 
+                okapi::Motor(CHASSIS_MOTORPORT_RB, 
+                true, okapi::AbstractMotor::gearset::green,
+                okapi::AbstractMotor::encoderUnits::degrees)});
+        okapi::MotorGroup leftBase({okapi::Motor(CHASSIS_MOTORPORT_LF, 
+                false, okapi::AbstractMotor::gearset::green,
+                okapi::AbstractMotor::encoderUnits::degrees), 
+                okapi::Motor(CHASSIS_MOTORPORT_LB, 
+                false, okapi::AbstractMotor::gearset::green,
+                okapi::AbstractMotor::encoderUnits::degrees)});
 
-        auto chassis = okapi::ChassisControllerFactory::create(
-            {CHASSIS_MOTORPORT_LF, CHASSIS_MOTORPORT_LB},
-            {-CHASSIS_MOTORPORT_RF, -CHASSIS_MOTORPORT_RB},
-            okapi::AbstractMotor::gearset::green, {4_in, 13.25_in});
-
-        okapi::Motor rfMotor(CHASSIS_MOTORPORT_RF, true,
-        okapi::AbstractMotor::gearset::green, okapi::AbstractMotor::encoderUnits::degrees);
-
-        okapi::Motor lfMotor(CHASSIS_MOTORPORT_LF, false,            
-        okapi::AbstractMotor::gearset::green, okapi::AbstractMotor::encoderUnits::degrees);
-
-        okapi::Motor lbMotor(CHASSIS_MOTORPORT_LB, false,            
-        okapi::AbstractMotor::gearset::green, okapi::AbstractMotor::encoderUnits::degrees);
-
-        okapi::Motor rbMotor(CHASSIS_MOTORPORT_RB, true,            
-        okapi::AbstractMotor::gearset::green, okapi::AbstractMotor::encoderUnits::degrees);
     }
 
 void Chassis::driver(okapi::Controller controller) {
     double left = controller.getAnalog(okapi::ControllerAnalog::leftY);
     double right = controller.getAnalog(okapi::ControllerAnalog::rightY);
-    chassis.tank(left, right, 0.05);
+    leftBase.controllerSet(left);
+    rightBase.controllerSet(right);
 }
 
-void Chassis::turnAngle(okapi::QAngle angle) {
-    chassis.turnAngle(angle);
+void Chassis::moveDistance(float leftTarg, float rightTarg){
+    //leftTarg *= 90;
+    //rightTarg *= 90;
+    leftBase.tarePosition();
+    rightBase.tarePosition();
+    float leftError = leftTarg - leftBase.getPosition(); 
+    float rightError = rightTarg - rightBase.getPosition(); 
+    while(abs(leftError) > 20){
+        pros::delay(20);
+        float leftOutput = leftError * 0.5;
+        float rightOutput = rightError * 0.5;
+        leftBase.moveVelocity(leftOutput);
+        rightBase.moveVelocity(rightOutput);
+        float leftError = leftTarg - leftBase.getPosition(); 
+        float rightError = rightTarg - rightBase.getPosition(); 
+    }
+    leftBase.moveVelocity(0);
+    rightBase.moveVelocity(0);
 }
 
-void Chassis::moveDistance(okapi::QLength distance, double velocity){
-    chassis.setMaxVelocity(velocity);
-    chassis.moveDistance(distance);
-    chassis.setMaxVelocity(200);
-}
-
-void Chassis::moveSide(char side, double arcLength){
+void Chassis::turnAngle(float angle){
     
-    if(side == 'l'){
+}
 
-    }
-    else if(side == 'r'){
-        
-    }
+void Chassis::setVelocity(int velo){
+    leftBase.moveVelocity(velo);
+    rightBase.moveVelocity(velo);
 }
